b9dd6bc2fbf1bb3db13118152d28586fb4525856
@pipeprograming2819 <hnofelipeangulo@gmail.com>
2022-06-30 17:39:23 -0500

    Estructure TestCase Modify

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,11 +3,7 @@ describe('Ordering Page Login', () => {
         cy.visit('https://automation-wappi.vercel.app/login');
     });
     context('When the user enters the data', () => {
-        before(() => {
-            cy.get('#username').type('Feliangu14');
-            cy.get('#password').type('Feliangu14@');
-            cy.get('#button-login').click();
-        });
+
         it('Next', () => {});
     });
 });
