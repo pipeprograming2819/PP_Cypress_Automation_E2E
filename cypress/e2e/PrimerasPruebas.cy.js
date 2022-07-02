339743abf95ea4aeb24065554254f943e0591415
@pipeprograming2819 <hnofelipeangulo@gmail.com>
2022-07-02 05:31:09 -0500

    automation tescase contabilizar elementos home page

--- cypress/e2e/PrimerasPruebas.cy.js
+++ cypress/e2e/PrimerasPruebas.cy.js
@@ -1,12 +1,14 @@
+/// <reference types="cypress"/>
 //Suite de casos que contiene cada caso
 describe('Primer Conjunto de Casos de Prueba', function() {
     //Caso de prueba 1
-    it('Ingresar a la pagina principal de Atenea', function() {
-        //paso 1
-        //paso 2
-        //paso 3
+    it('Contabilizar la cantidad de elemento en la sesion de pagina principal', function() {
+
+        //Ingresamos a la pagina
         cy.visit("http://automationpractice.com/index.php");
 
+        //Verificar la cantidad de elementos disponibles
+        cy.get('#homefeatured.produc-container').should('have.', 7)
     });
     //Caso de prueba  2
     //Caso de prueba  3   
