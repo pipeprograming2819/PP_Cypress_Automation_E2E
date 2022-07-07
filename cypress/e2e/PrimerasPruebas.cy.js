f579cd4eb0f01ba0b08180a46a07539f9fd7c5b4
@pipeprograming2819 <hnofelipeangulo@gmail.com>
2022-07-07 14:56:03 -0500

    Cantidad de elements por parametros

--- cypress/e2e/PrimerasPruebas.cy.js
+++ cypress/e2e/PrimerasPruebas.cy.js
@@ -10,8 +10,14 @@ describe('Primer Conjunto de Casos de Prueba', function() {
         //Verificar la cantidad de elementos disponibles
         cy.get('#homefeatured .product-container').should('have.length', 7);
 
+        //Obtenemos el element homefeatured .product-container como un parametro
 
+        cy.get('#homefeatured .product-container').as('ProductosPopulares');
+
+        //Verificamos  nuevamente la cantidad de elementos utilizando el parametro
+        cy.get('@ProductosPopulares').should('have.length', 7);
     });
+
     //Caso de prueba  2
     //Caso de prueba  3   
 
