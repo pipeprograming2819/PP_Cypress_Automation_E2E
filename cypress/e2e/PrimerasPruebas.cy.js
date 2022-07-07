927cb8594b22427b537137d21a12d89498ac3abe
@pipeprograming2819 <hnofelipeangulo@gmail.com>
2022-07-07 17:06:21 -0500

    Se agrega un elemento para encontrar los precios del producto

--- cypress/e2e/PrimerasPruebas.cy.js
+++ cypress/e2e/PrimerasPruebas.cy.js
@@ -1,11 +1,14 @@
 /// <reference types="cypress"/>
 //Suite de casos que contiene cada caso
 describe('Primer Conjunto de Casos de Prueba', function() {
-    //Caso de prueba 1
-    it('Contabilizar la cantidad de elemento en la sesion de pagina principal', function() {
 
+    beforeEach(() => {
         //Ingresamos a la pagina
         cy.visit("http://automationpractice.com/index.php");
+    })
+
+    //Caso de prueba 1
+    it('Contabilizar la cantidad de elemento en la sesion de pagina principal', function() {
 
         //Verificar la cantidad de elementos disponibles
         cy.get('#homefeatured .product-container').should('have.length', 7);
@@ -20,9 +23,6 @@ describe('Primer Conjunto de Casos de Prueba', function() {
 
     //Caso de prueba  2
     it('Agregar el elemento tipo blouse al carro de compras desde la pagina principal', function() {
-
-        //Ingresamos a la pagina
-        cy.visit("http://automationpractice.com/index.php");
 
         cy.get('#homefeatured .product-container').as('ProductosPopulares');
 
@@ -40,6 +40,29 @@ describe('Primer Conjunto de Casos de Prueba', function() {
 
 
     });
-    //Caso de prueba  3   
+    //Caso de prueba  3
+    it('Agregar el elemento tipo "Printed Dress" al carro de compras desde la pagina principal', function() {
+
+        cy.get('#homefeatured .product-container').as('ProductosPopulares');
+
+        // iteramos para encontrar un producto con un nombre x
+        cy.get('@ProductosPopulares')
+            .find('.product-name')
+            .each(($el, index, $list) => {
+
+                cy.get('@ProductosPopulares').eq(index).find('.price').then(function($el1) {
+                    let precio = $el1.text()
+                    cy.log(precio)
+
+                    if ($el.attr('title') === 'Printed Dress' && precio.includes('26.00')) {
+                        cy.log('Se ha encontrado el elemento buscado');
+                        cy.log('Se ha encontrado el precio buscado');
+                        cy.get('@ProductosPopulares').eq(index).contains('Add to cart').click();
+                    }
+                })
+            })
+
+    });
+    //Caso de prueba  4       
 
 });
